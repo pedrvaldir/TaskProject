cb8fd8aa5717d1c65578629719b6fa27af596c64
Valdir <pedrvaldir@gmail.com>
2019-05-20 05:36:50 -0300

    create activity loginUser

---
+++ app/src/main/java/com/taskproject/valdir/taskproject/views/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.taskproject.valdir.taskproject.views
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.taskproject.valdir.taskproject.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
