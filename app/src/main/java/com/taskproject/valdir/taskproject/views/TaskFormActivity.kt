5151d21d1a8dada6115b61268c103c53000dfb1c
Valdir <pedrvaldir@gmail.com>
2019-05-21 04:20:50 -0300

    Create activity task form

---
+++ app/src/main/java/com/taskproject/valdir/taskproject/views/TaskFormActivity.kt
@@ -0,0 +1,13 @@
+package com.taskproject.valdir.taskproject.views
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.taskproject.valdir.taskproject.R
+
+class TaskFormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_form)
+    }
+}
