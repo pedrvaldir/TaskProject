9f64887092b88059049374fec816b0d7d2a51af5
Valdir <pedrvaldir@gmail.com>
2019-05-22 02:40:43 -0300

    Create methods get and insert in TaskRepository

--- app/src/main/java/com/taskproject/valdir/taskproject/repository/TaskRepository.kt
+++ app/src/main/java/com/taskproject/valdir/taskproject/repository/TaskRepository.kt
@@ -1,5 +1,6 @@
 package com.taskproject.valdir.taskproject.repository
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
 import com.taskproject.valdir.taskproject.constants.DataBaseConstants
@@ -21,6 +22,56 @@ class TaskRepository private constructor(context: Context) {
 
         private var INSTANCE: TaskRepository? = null
 
+    }
+
+    //get, update, insert e delete  - CRUD
+    fun get(id: Int): TaskEntity? {
+
+        var taskEntity: TaskEntity? = null
+
+        try {
+            val cursor: Cursor
+
+            val db = mTaskDataBaseHelper.readableDatabase
+            val projection = arrayOf(
+                DataBaseConstants.TASK.COLUMNS.ID,
+                DataBaseConstants.TASK.COLUMNS.USERID,
+                DataBaseConstants.TASK.COLUMNS.PRIORITYID,
+                DataBaseConstants.TASK.COLUMNS.DESCRIPTION,
+                DataBaseConstants.TASK.COLUMNS.DUEDATE,
+                DataBaseConstants.TASK.COLUMNS.COMPLETE
+            )
+            //Selection o Id = ? -> coringa
+            val selection =
+                "${DataBaseConstants.TASK.COLUMNS.ID} = ?"
+            // convertido para string, pois selectoinARgs espera Arraylist
+            val selectionArgs = arrayOf(id.toString())
+
+            //outra forma de consultar dados :         db.rawQuery("select * from user where email = gabriel", null)
+            //Retorno de query cursor
+            cursor = db.query(DataBaseConstants.USER.TABLE_NAME, projection, selection, selectionArgs, null, null, null)
+
+            if (cursor.count > 0) {
+                //mover para primeiro registro
+                cursor.moveToFirst()
+
+                val taskId = cursor.getInt(cursor.getColumnIndex(DataBaseConstants.TASK.COLUMNS.ID))
+                val userId = cursor.getInt(cursor.getColumnIndex(DataBaseConstants.TASK.COLUMNS.USERID))
+                val priorityId = cursor.getInt(cursor.getColumnIndex(DataBaseConstants.TASK.COLUMNS.PRIORITYID))
+                val description = cursor.getString(cursor.getColumnIndex(DataBaseConstants.TASK.COLUMNS.DESCRIPTION))
+                val dueData = cursor.getString(cursor.getColumnIndex(DataBaseConstants.TASK.COLUMNS.DUEDATE))
+                // dessa forma se o valor de retorno for igual a 1 complete será true
+                val complete = (cursor.getInt(cursor.getColumnIndex(DataBaseConstants.TASK.COLUMNS.COMPLETE)) == 1)
+
+                //preencho a entidade task
+                taskEntity = TaskEntity(id, userId, priorityId, description, dueData, complete)
+            }
+
+            cursor.close()
+        } catch (e: Exception) {
+            return taskEntity
+        }
+        return taskEntity
     }
 
     fun getList(userId: Int): MutableList<TaskEntity> {
@@ -59,5 +110,21 @@ class TaskRepository private constructor(context: Context) {
 
         return list
 
+    }
+
+    fun insert(task: TaskEntity) {
+        val db = mTaskDataBaseHelper.writableDatabase
+
+        // se por acaso for true assuma o valor 1
+        val complete: Int = if (task.complete) 1 else 0
+
+        val insertValues = ContentValues()
+        insertValues.put(DataBaseConstants.TASK.COLUMNS.USERID, task.userId)
+        insertValues.put(DataBaseConstants.TASK.COLUMNS.PRIORITYID, task.priorityId)
+        insertValues.put(DataBaseConstants.TASK.COLUMNS.DESCRIPTION, task.description)
+        insertValues.put(DataBaseConstants.TASK.COLUMNS.DUEDATE, task.dueData)
+        insertValues.put(DataBaseConstants.TASK.COLUMNS.COMPLETE, complete)
+
+        db.insert(DataBaseConstants.TASK.TABLE_NAME, null, insertValues)
     }
 }
